8e293f391891f9a3d134a4e1c9bbac8f2b99db5e
Jeremy Carroll <jcarroll@coursera.org>
2025-03-13 03:24:20 -0700

    extra test

--- test/test_venn6.c
+++ test/test_venn6.c
@@ -29,6 +29,14 @@ static void found_solution()
   printSolution(NULL);
   printStatisticsFull();
 #endif
+  /*
+    uint32_t i;
+    u_int32_t mask = 3;
+    for (i = 0, mask = 3; i < NCURVES - 1; i++, mask <<= 1) {
+      printf("%d,", g_faces[(NFACES - 1) & ~mask].cycle->length);
+    }
+    printf("%d\n", g_faces[((NFACES - 1) >> 1) - 1].cycle->length);
+    */
   solution_count++;
 }
 
@@ -53,9 +61,31 @@ static void test_search_for_best_solution()
   TEST_ASSERT_EQUAL(160, solution_count);
 }
 
+static void test_search_for_two_solutions()
+{
+  solution_count = 0;
+  setupCentralFaces(5, 5, 5, 4, 4, 4);
+  addSpecificFace("c", "adbce");
+  /* This is a short statement of the best solution.
+  addSpecificFace("a", "abed");
+  addSpecificFace("c", "adbce");
+  addSpecificFace("bde", "bfd");
+  addSpecificFace("bdf", "abf");
+  addSpecificFace("abce", "cef");
+  addSpecificFace("acdf", "aced");
+  addSpecificFace("cde", "adb");
+  addSpecificFace("b", "abcf");
+  addSpecificFace("f", "aefdc");
+  */
+  search(true, found_solution);
+
+  TEST_ASSERT_EQUAL(2, solution_count);
+}
+
 int main(void)
 {
   UNITY_BEGIN();
   RUN_TEST(test_search_for_best_solution);
+  RUN_TEST(test_search_for_two_solutions);
   return UNITY_END();
 }
