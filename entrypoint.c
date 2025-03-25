410a5d87df04ed3cc6e3d6f3c7aee5b069b547e8
Jeremy Carroll <jcarroll@coursera.org>
2025-03-25 05:55:47 -0700

    refactor: remove unused Unity test functions from entrypoint.c

--- entrypoint.c
+++ entrypoint.c
@@ -1,13 +1,12 @@
-
-#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "core.h"
+#include "face.h"
+#include "main.h"
+#include "search.h"
 
 extern int dynamicMain0(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) { return dynamicMain0(argc, argv); }
-
-/* These two functions are used in the unit tests from debug.c, which is
-   included in the main runtime, but unused.
-*/
-void UnityAssertEqualNumber() { assert(0); }
-void UnityFail() { assert(0); }
