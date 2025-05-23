8efd34850d01ce1f74e72dddf69a48b4466aefcf
Jeremy Carroll <jcarroll@coursera.org>
2025-05-22 18:35:52 -0700

    missing files

---
+++ visible_for_testing.h
@@ -0,0 +1,13 @@
+
+/* Copyright (C) 2025 Jeremy J. Carroll. See LICENSE for details. */
+
+#ifndef VISIBLE_FOR_TESTING_H
+#define VISIBLE_FOR_TESTING_H
+
+#include "core.h"
+
+/* Internal declarations that should be visible to test files will go here */
+extern FACE searchChooseNextFace(void);
+extern uint64 CycleGuessCounter ;
+
+#endif  // VISIBLE_FOR_TESTING_H 
