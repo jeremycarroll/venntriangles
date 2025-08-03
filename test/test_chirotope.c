4e80b13c90c508ec6033597f9cc3a23bf8adf085
Jeremy Carroll <jcarroll@coursera.org>
2025-08-03 13:11:41 -0700

    copy testMeggido to test_chirotope

--- test/test_chirotope.c
+++ test/test_chirotope.c
@@ -103,6 +103,57 @@ static void testInconsistent(void)
   RUN_CHIROTOPE_TEST(6, "-+--+?-+++++?++++++?", false, false, true);
 }
 
+/* The same predicate as per testMeggido in PCO 5 is extendible as
+   a partial chirotope.
+ */
+static void testMegiddo(int a, int b, int c, int d, int e, int f, int g, int h,
+                        int i, int j, int k, int l, int m)
+{
+  AlternatingPredicate megiddo = CREATE_CHIROTOPE(13);
+  initializeAlternating(megiddo);
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, a, c, d));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, b, d, e));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, c, e, f));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, d, f, g));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, e, g, h));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, f, h, a));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, g, a, c));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, h, c, b));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, a, b, i));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, c, i, j));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, b, j, k));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, i, k, l));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, j, l, m));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, k, m, a));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, l, a, b));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, m, b, c));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, h, c, m));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingSet(megiddo, b, h, m));
+  TEST_ASSERT_EQUAL(true, dynamicAlternatingClosure(megiddo));
+  int counter = EngineCounter;
+  bool result = dynamicAlternatingComplete(megiddo);
+  printf("Engine counter = %d\n", EngineCounter - counter);
+  TEST_ASSERT_MESSAGE(result, "extendable");
+}
+
+static void runTestMegiddoA(void)
+{
+  testMegiddo(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+}
+
+static void runTestMegiddoB(void)
+{
+  testMegiddo(4, 5, 0, 1, 14, 3, 6, 13, 8, 9, 10, 11, 12);
+}
+static void runTestMegiddoC(void)
+{
+  testMegiddo(9, 10, 11, 4, 5, 0, 1, 14, 3, 6, 13, 8, 12);
+}
+static void runTestMegiddoD(void)
+{
+  testMegiddo(9, 10, 11, 6, 13, 8, 12, 4, 5, 0, 1, 14, 3);
+}
+
 int main(void)
 {
   UNITY_BEGIN();
@@ -112,5 +163,6 @@ int main(void)
   RUN_TEST(testSimple);
   RUN_TEST(testSimpleInconsistent);
   RUN_TEST(testSimpleIncomplete);
+  RUN_TEST(runTestMegiddoA);
   return UNITY_END();
 }
